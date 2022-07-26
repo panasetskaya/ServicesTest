cf1fb4cce774cd77c5fe0d4c2ccd80d12a4c2b5f
Maria Panasetskaia <mariapolikutina@gmail.com>
2022-07-26 13:59:00 +0800

    add Worker

--- app/src/main/java/ru/sumin/servicestest/MyJobService.kt
+++ app/src/main/java/ru/sumin/servicestest/MyJobService.kt
@@ -1,13 +1,9 @@
 package ru.sumin.servicestest
 
-import android.app.Service
 import android.app.job.JobParameters
 import android.app.job.JobService
-import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.os.IBinder
-import android.os.PersistableBundle
 import android.util.Log
 import kotlinx.coroutines.*
 
