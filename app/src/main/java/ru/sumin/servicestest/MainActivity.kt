e1117b4d47d4d5edc44b79cab11023643759f6ec
Maria Panasetskaia <mariapolikutina@gmail.com>
2022-09-13 14:18:01 +0800

    add AlarmReceiver (AlarmManager)

--- app/src/main/java/ru/sumin/servicestest/MainActivity.kt
+++ app/src/main/java/ru/sumin/servicestest/MainActivity.kt
@@ -1,5 +1,7 @@
 package ru.sumin.servicestest
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.app.job.JobInfo
 import android.app.job.JobScheduler
 import android.app.job.JobWorkItem
@@ -14,6 +16,7 @@ import androidx.core.content.ContextCompat
 import androidx.work.ExistingWorkPolicy
 import androidx.work.WorkManager
 import ru.sumin.servicestest.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -73,6 +76,14 @@ class MainActivity : AppCompatActivity() {
                 ExistingWorkPolicy.APPEND,
                 MyWorker.makeRequest(page++)
             )
+        }
+        binding.alarmManager.setOnClickListener {
+            val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+            val calendar = Calendar.getInstance()
+            calendar.add(Calendar.SECOND,10)
+            val intent = AlarmReceiver.newIntent(this)
+            val pendingIntent = PendingIntent.getBroadcast(this,100,intent,0)
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP,calendar.timeInMillis,pendingIntent)
         }
     }
 
